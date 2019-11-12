96b0d29ad18b5ae9423382e30cf88438ea8e7b9a
Safeguard14 <35404422+Safeguard14@users.noreply.github.com>
2019-11-12 23:05:05 +0100

    Add UserRepository as an example

---
+++ ProjektBazyDanych/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektBazyDanych.Repository.Interface
+{
+    interface IUserRepository : IGenericRepository<User>
+    {
+        User GetByEmail(string mail);
+    }
+}
---
+++ ProjektBazyDanych/Repository/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProjektBazyDanych.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektBazyDanych.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public User GetByEmail(string mail)
+        {
+            return _context.Users.Where(x => x.email == mail).SingleOrDefault();
+        }
+    }
+}
