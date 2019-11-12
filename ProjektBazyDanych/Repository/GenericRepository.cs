96a7913b7ccee7ab35525e6053606e7735002c9c
Safeguard14 <35404422+Safeguard14@users.noreply.github.com>
2019-11-12 22:50:12 +0100

    Add GenericRepository with Interface

---
+++ ProjektBazyDanych/Repository/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using ProjektBazyDanych.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjektBazyDanych.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public connectionString _context = null;
+        public DbSet<T> table = null;
+        public GenericRepository()
+        {
+            this._context = new connectionString();
+            table = _context.Set<T>();
+        }
+        public GenericRepository(connectionString _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+
+}
---
+++ ProjektBazyDanych/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ProjektBazyDanych.Repository.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(object id);
+
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Delete(object id);
+
+        void Save();
+    }
